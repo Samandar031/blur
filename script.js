e07563fc5188c1fc16b476eeac1a965bda7ad2af
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-30 22:28:58 +0500

    k

--- script.js
+++ script.js
@@ -17,29 +17,30 @@ let foiz = document.querySelector(".foiz");
 
 btn.addEventListener("click", function () {
   let t = setInterval(function () {
-    foiz.textContent = parseFloat(foiz.textContent) + 1;
+    foiz.textContent = parseFloat(foiz.textContent) - 1;
     let kamayuvchi = foiz.textContent;
 
+    img.style.filter = `blur(${kamayuvchi}rem)`;
+
     if (kamayuvchi == 100) {
-      img.style.filter = `blur(1.1rem)`;
-    } else if (kamayuvchi == 90) {
-      img.style.filter = `blur(0.9rem)`;
-    } else if (kamayuvchi == 80) {
-      img.style.filter = `blur(0.8rem)`;
-    } else if (kamayuvchi == 60) {
-      img.style.filter = `blur(0.6rem)`;
-    } else if (kamayuvchi == 40) {
-      img.style.filter = `blur(0.4rem)`;
-    } else if (kamayuvchi == 20) {
-      img.style.filter = `blur(0.2rem)`;
-    } else if (kamayuvchi == 10) {
-      img.style.filter = `blur(0.1rem)`;
-    } else if (kamayuvchi == 0) {
-      img.style.filter = `blur(0)`;
-      clearTimeout(t);
-    } else if (kamayuvchi < 0) {
+      // } else if (kamayuvchi == 90) {
+      //   img.style.filter = `blur(0.9rem)`;
+      // } else if (kamayuvchi == 80) {
+      //   img.style.filter = `blur(0.8rem)`;
+      // } else if (kamayuvchi == 60) {
+      //   img.style.filter = `blur(0.6rem)`;
+      // } else if (kamayuvchi == 40) {
+      //   img.style.filter = `blur(0.4rem)`;
+      // } else if (kamayuvchi == 20) {
+      //   img.style.filter = `blur(0.2rem)`;
+      // } else if (kamayuvchi == 10) {
+      //   img.style.filter = `blur(0.1rem)`;
+      // } else if (kamayuvchi == 0) {
+      //   img.style.filter = `blur(0)`;
+      //   clearTimeout(t);
+    } else if ((kamayuvchi = 0)) {
       clearTimeout(t);
     }
-    console.log(kamayuvchi);
+    // console.log(kamayuvchi);
   }, 50);
 });
